1264376ae318a06b820273c00e26cfacfe37baa0
Daren Schaad <darenschaad@gmail.com>
2016-03-18 16:04:26 -0700

    Fix spacing and indentation

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,13 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
-
-
 exports.getRepos = function(userName){
   $.get('https://api.github.com/users/'+ userName +  '/repos?access_token=' + apiKey).then(function(response){
       $('#gitName').html("User Name: " + response[0].owner.login)
       $('#repos').empty();
-      // $('#gitPic').empty();
-      // $('#gitPic').show();
       $('#gitPic').html("<img src='" + response[0].owner.avatar_url +  "'alt='github user avatar'/><br>");
       $('#repos').html("<h3>Repositories</h3>");
       $('#created').html("<h3>Created On</h3>");
@@ -21,8 +17,6 @@ exports.getRepos = function(userName){
     for (var k = 0 ; k < response.length ; k++){
       $('#updated').append(moment(response[k].pushed_at).format("MMM Do YYYY") + "<br>");
     }
-    console.log(moment(response[0].created_at).calendar());
-    // console.log(JSON.stringify(response));
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
